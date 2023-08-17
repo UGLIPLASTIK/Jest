03b8fdd1ab511f8c4085968afc1ede587cb4c273
Dmitry.Kononets <d.cononec@gmail.com>
2023-08-17 18:58:07 +0300

    coverage

--- src/__tests__/basic.js
+++ src/__tests__/basic.js
@@ -7,7 +7,7 @@ test('basic test', () => {
   expect(result).toBe(4);
 });
 
-test('calculateTotal sum', () => {
+test('calculateTotal sum without discount', () => {
   const list = [
     {
       id: 456,
@@ -25,4 +25,24 @@ test('calculateTotal sum', () => {
 
   const result = calculateTotal(list);
   expect(result).toBe(2500);
+});
+
+test('calculateTotal sum with discount', () => {
+  const list = [
+    {
+      id: 456,
+      name: 'Война и мир',
+      count: 3,
+      price: 401,
+    },
+    {
+      id: 77,
+      name: 'JavaSctipt',
+      count: 1,
+      price: 1300,
+    },
+  ];
+
+  const result = calculateTotal(list, true);
+  expect(result).toBeCloseTo(2230.173);
 });
--- src/calculate.js
+++ src/calculate.js
@@ -1,7 +1,14 @@
+/* eslint-disable arrow-body-style */
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-plusplus */
-export function calculateTotal(purchases) {
-  return purchases.reduce((total, item) => total + (item.count * item.price), 0);
+export function calculateTotal(purchases, applyDiscount) {
+  const sum = purchases.reduce((total, item) => {
+    return total + (item.count * item.price);
+  }, 0);
+  if (applyDiscount) {
+    return sum * 0.891;
+  }
+  return sum;
 }
